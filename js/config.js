9e878de94007afb2d0fdb4d078b1bad7ce99a0b6
NTFLNetwork <gavin.hysell@icloud.com>
2026-08-01 10:56:26 -0400

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,8 @@
+export const SUPABASE_URL = "https://zggrwyxtakqpqyrxskiq.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_bPPwe-rurJY0mf5bE5RFTw_MPKRNB0_";
+export const SEASON_LABEL = "Season 3";
+export const APP_NAME = "NTFL Network";
+export const LEAGUE_LOGO = "./assets/league-logo.jpeg";
+export const DEMO_EMAIL = "demo@ntflnetwork.com";
+export const DEMO_PASSWORD = "NTFLdemo2026!";
+export const DEMO_FLAG = "ntfl_demo_admin";
